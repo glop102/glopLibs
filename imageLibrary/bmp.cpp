38f040bba294474d9ab1161e4c37d12d328a8d85
glop102 <alexlikeschess@gmail.com>
2016-09-29 02:56:54 -0400

    Full Rewrite of BMP to be correct this time

--- imageLibrary/bmp.cpp
+++ imageLibrary/bmp.cpp
@@ -1,16 +1,15 @@
 #include "bmp.h"
 
+#define BMP_DEBUG
+
 namespace GLOP_IMAGE_BMP{
 	unsigned long distToPixels = 0;
 	unsigned int headerSize = 0;
 	unsigned int bitsPerPixel=0;
 	unsigned int compressionMethod=0;
 	unsigned int colorTableLength=0;
-	unsigned int RED_MASK=0; // mask of the bytes that the pixels are to get out this channel
-	unsigned int GREEN_MASK=0;
-	unsigned int BLUE_MASK=0;
-	unsigned int ALPHA_MASK=0;
 	bool upsideDownRows = true; // it is ussually true
+	std::vector<Pixel> colorTable;
 
 	bool validBMP(unsigned char *filebuffer){
 		if(filebuffer[0] != 'B' || filebuffer[1]!='M')
@@ -41,10 +40,6 @@ namespace GLOP_IMAGE_BMP{
 		bitsPerPixel=0;
 		compressionMethod=0;
 		colorTableLength=0;
-		RED_MASK=0; // mask of the bytes that the pixels are to get out this channel
-		GREEN_MASK=0;
-		BLUE_MASK=0;
-		ALPHA_MASK=0;
 		upsideDownRows = true; // it is ussually true
 
 		//start by reading the header and get the basic info about the image
@@ -53,12 +48,12 @@ namespace GLOP_IMAGE_BMP{
 			case  12: // windows 2
 			case  64: // OS/2 v2
 			case  16: // OS/2 v2 special case
-			case  40: // Windows NT 3.1x
 			case  52: // undocumented
 			case  56: // unofficial but seen on adobe forum
 			case 108: // Windows NT 4.0 , Win95
 				printf("BitMap Header Version too old\nNot Decoding Image\n");
 				return;
+			case  40: // Windows NT 3.1x
 			case 124: // Windows NT 5.0 , Win98
 				x+=4; // get past the bytes that say how large the header is
 				headerSize-=4;
@@ -66,7 +61,7 @@ namespace GLOP_IMAGE_BMP{
 				data->width = (filebuffer[x+0]<<(8*0)) | (filebuffer[x+1]<<(8*1)) | (filebuffer[x+2]<<(8*2)) | (filebuffer[x+3]<<(8*3));
 				x+=4; headerSize-=4;
 				{ // bmp files are allowed to have negative heights
-					int heightTemp = (filebuffer[x+0]<<(8*0)) | (filebuffer[x+1]<<(8*1)) | (filebuffer[x+2]<<(8*2)) | (filebuffer[x+3]<<(8*3));
+					int32_t heightTemp = (filebuffer[x+0]<<(8*0)) | (filebuffer[x+1]<<(8*1)) | (filebuffer[x+2]<<(8*2)) | (filebuffer[x+3]<<(8*3));
 					if(heightTemp>=0)
 						data->height = heightTemp;
 					else{
@@ -81,11 +76,52 @@ namespace GLOP_IMAGE_BMP{
 				x+=2; headerSize-=2;
 				bitsPerPixel = (filebuffer[x+0]<<(8*0)) | (filebuffer[x+1]<<(8*1)) ; 
 				x+=2; headerSize-=2;
+				switch(bitsPerPixel){
+					case 1:
+					case 2:
+					case 4:
+					case 8:
+					case 16:
+					case 24:
+					case 32:
+						break;
+					default:
+						printf("Incorrect bitsPerPixel value: %d\n",bitsPerPixel);
+						data->height=0;
+						data->width=0;
+						return;
+				}
+				if(bitsPerPixel == 32)
+					data->pixelType = PixelType::RGB_ALPHA;
+				else
+					data->pixelType = PixelType::RGB; // assume is RGB until otherwise
 
 				compressionMethod = (filebuffer[x+0]<<(8*0)) | (filebuffer[x+1]<<(8*1)) | (filebuffer[x+2]<<(8*2)) | (filebuffer[x+3]<<(8*3));
 				x+=4; headerSize-=4;
+				switch(compressionMethod){
+					case 1: // RunLengthEncoding 8BIT
+					case 2: // RunLengthEncoding 4BIT
+					case 3: // BitMasks - a bit mask for each channel is defined for the pixel size
+					case 4: // Jpeg Image
+					case 5: // PNG Image
+					case 6: // Win Only
+					case 11: // Win Only
+					case 12: // Win Only
+					case 13: // Win Only
+						printf("Unsuported Compression Method\n");
+						data->height=0;
+						data->width=0;
+						return;
+					case 0: // no compression
+						break;
+					default:
+						printf("Unknown Compression Method\n");
+						data->height=0;
+						data->width=0;
+						return;
+				}
 
-				//image size
+				//image size - raw bytes size of the whole pixel array
 				x+=4; headerSize-=4;
 				//x meters per pixel
 				//y meters per pixel
@@ -94,18 +130,25 @@ namespace GLOP_IMAGE_BMP{
 				colorTableLength = (filebuffer[x+0]<<(8*0)) | (filebuffer[x+1]<<(8*1)) | (filebuffer[x+2]<<(8*2)) | (filebuffer[x+3]<<(8*3));
 				x+=4; headerSize-=4;
 
-				//important color count
+				//important color count - we can ignore this one
 				x+=4;
 				headerSize-=4;
 
-				RED_MASK = (filebuffer[x+0]<<(8*0)) | (filebuffer[x+1]<<(8*1)) | (filebuffer[x+2]<<(8*2)) | (filebuffer[x+3]<<(8*3));
-				x+=4; headerSize-=4;
-				GREEN_MASK = (filebuffer[x+0]<<(8*0)) | (filebuffer[x+1]<<(8*1)) | (filebuffer[x+2]<<(8*2)) | (filebuffer[x+3]<<(8*3));
-				x+=4; headerSize-=4;
-				BLUE_MASK = (filebuffer[x+0]<<(8*0)) | (filebuffer[x+1]<<(8*1)) | (filebuffer[x+2]<<(8*2)) | (filebuffer[x+3]<<(8*3));
-				x+=4; headerSize-=4;
-				ALPHA_MASK = (filebuffer[x+0]<<(8*0)) | (filebuffer[x+1]<<(8*1)) | (filebuffer[x+2]<<(8*2)) | (filebuffer[x+3]<<(8*3));
-				x+=4; headerSize-=4;
+				//now lets jump to the color table
+				x+=headerSize;
+				headerSize=0;
+
+				colorTable.clear(); // lets reset this baby and start fresh for the new image
+				colorTable.reserve(colorTableLength);
+				while(x<distToPixels){
+					Pixel temp;
+					temp.B=filebuffer[x + 0]; // colors are in the table as BGR
+					temp.G=filebuffer[x + 1];
+					temp.R=filebuffer[x + 2];
+					temp.A=filebuffer[x + 3];
+					if(temp.A != 0) data->pixelType=RGB_ALPHA;
+					x+=4;
+				}
 				
 				#ifdef BMP_DEBUG
 					printf("Header Data\n");
@@ -114,99 +157,87 @@ namespace GLOP_IMAGE_BMP{
 					printf("PixelBits\t: %d\n",bitsPerPixel);
 					printf("Compression\t: %d\n",compressionMethod);
 					printf("Color Table\t: %d\n",colorTableLength);
-					printf("RED_MASK\t: %x\n",RED_MASK);
-					printf("GREEN_MASK\t: %x\n",GREEN_MASK);
-					printf("BLUE_MASK\t: %x\n",BLUE_MASK);
-					printf("ALPHA_MASK\t: %x\n",ALPHA_MASK);
+					for(int y=0;y<colorTable.size();y++){
+						Pixel& t = colorTable[y];
+						printf("\tR%d  G%d  B%d  A%d\n",t.R,t.G,t.B,t.A);
+					}
 				#endif
 
-				//blahblahblah
-				//color spaces finish out the rest of the header
-
 				break;
+			default:
+				printf("Cant make sense of bitmap Information Header\n");
+				return;
 		}
 
-		if(ALPHA_MASK != 0)
-			data->pixelType = PixelType::RGB_ALPHA;
-		else
-			data->pixelType = PixelType::RGB;
-
-		if(colorTableLength != 0){
-			//make color table stuffs some time later
-			x = 14+filebuffer[14]; // jump to after the header, where the color table is
-		}else{
-			x = 14+distToPixels; // Now, lets skip ahead to the pixels (header size + file descriptor size)
-			decodePixelArray(x,filebuffer,bufferLength,data);
-		}
+		x = distToPixels; // Now, lets skip ahead to the pixels (header size + file descriptor size)
+		decodePixelArray(filebuffer+x,bufferLength,data);
 
 	}
 
-	void decodePixelArray(unsigned int offset, unsigned char* filebuffer,int bufferLength, ImageData* data){
+	void decodePixelArray(unsigned char* filebuffer,int bufferLength, ImageData* data){
 		//==============================================
 		// ACTUAL DECODING OF PIXELS NOW
 		//==============================================
-		// assuming no pallete and bitsize of pixels are 16,24,32
-
-		// how far do we need to shift each of the channels after the mask
-		uint8_t RED_SHIFT = 255;
-		uint8_t GREEN_SHIFT = 255;
-		uint8_t BLUE_SHIFT = 255;
-		uint8_t ALPHA_SHIFT = 255;
-		for(uint8_t x = 0 ; x<32 ; x++){
-			if(RED_SHIFT == 255 && (RED_MASK >> x) & 1)
-				RED_SHIFT = x;
-			if(GREEN_SHIFT == 255 && (GREEN_MASK >> x) & 1)
-				GREEN_SHIFT = x;
-			if(BLUE_SHIFT == 255 && (BLUE_MASK >> x) & 1)
-				BLUE_SHIFT = x;
-			if(ALPHA_SHIFT == 255 && (ALPHA_MASK >> x) & 1)
-				ALPHA_SHIFT = x;
-		}
 
 		//Scale all channels to be 8bit
 		data->bitDepth=8;
-		double RED_SCALE = 1.0;
-		double GREEN_SCALE = 1.0;
-		double BLUE_SCALE = 1.0;
-		double ALPHA_SCALE = 1.0;
-
-		if(RED_MASK   >> RED_SHIFT   != 0) RED_SCALE   = 0xff / (double)( RED_MASK   >> RED_SHIFT   );
-		if(GREEN_MASK >> GREEN_SHIFT != 0) GREEN_SCALE = 0xff / (double)( GREEN_MASK >> GREEN_SHIFT );
-		if(BLUE_MASK  >> BLUE_SHIFT  != 0) BLUE_SCALE  = 0xff / (double)( BLUE_MASK  >> BLUE_SHIFT  );
-		if(ALPHA_MASK >> ALPHA_SHIFT != 0) ALPHA_SCALE = 0xff / (double)( ALPHA_MASK >> ALPHA_SHIFT );
+		if(bitsPerPixel == 16){
+			printf("ERROR: WTF is 16 bits per pixel even mean?\n");
+			return;
+		}
 
 		// how long is each row of the pixel array in bytes
 		unsigned int rowWidth = ((bitsPerPixel * (data->width) + 31) / 32)*4;
 
 		#ifdef BMP_DEBUG
-			printf("RED_SHIFT\t: %d\n",RED_SHIFT);
-			printf("GREEN_SHIFT\t: %d\n",GREEN_SHIFT);
-			printf("BLUE_SHIFT\t: %d\n",BLUE_SHIFT);
-			printf("ALPHA_SHIFT\t: %d\n",ALPHA_SHIFT);
+			printf("\n");
 			printf("Row Width\t: %d\n",rowWidth);
+			printf("");
 		#endif
 
 		for(unsigned int row=0;row<data->height;row++){
 			for(unsigned int y=0;y<data->width;y++){
-				uint32_t pixelData = 0; // the bytes for this pixel
-				for(int b=0 ; b<bitsPerPixel/8 ; b++){
-					pixelData |= filebuffer[(row*rowWidth) + (y*bitsPerPixel/8) + b + offset] << (8*b);
-				}
-				Pixel* pix;
+				Pixel *pix;
 				if(upsideDownRows){
 					pix = & (data->pixels[((data->height-1) - row) *data->width + y]);
 				}else{
 					pix = & (data->pixels[row*data->width + y]);
 				}
-				pix->R = ((pixelData & RED_MASK)   >> RED_SHIFT  );
-				pix->G = ((pixelData & GREEN_MASK) >> GREEN_SHIFT);
-				pix->B = ((pixelData & BLUE_MASK)  >> BLUE_SHIFT );
-				pix->A = ((pixelData & ALPHA_MASK) >> ALPHA_SHIFT);
-
-				pix->R *= RED_SCALE;
-				pix->G *= GREEN_SCALE;
-				pix->B *= BLUE_SCALE;
-				pix->A *= ALPHA_SCALE;
+				switch(bitsPerPixel){
+					int x;
+					case 1:
+						x = filebuffer[row*rowWidth + (y/8)]; // get the byte that the curt pixel is in
+						x = ( x>>(7-(y%8)) ) & 1; // get the single bit out that represents our pixel
+						(*pix) = colorTable[x];
+						break;
+					case 2:
+						x = filebuffer[row*rowWidth + (y/4)]; // get the byte that the curt pixel is in
+						x = ( x>>(2*(3-(y%4))) ) & 3; // get the single bit out that represents our pixel
+						(*pix) = colorTable[x];
+						break;
+					case 4:
+						x = filebuffer[row*rowWidth + (y/2)]; // get the byte that the curt pixel is in
+						x = ( x>>(4*(1-(y%2))) ) & 0b1111; // get the single bit out that represents our pixel
+						(*pix) = colorTable[x];
+						break;
+					case 8:
+						x = filebuffer[row*rowWidth + y]; // get the byte that the curt pixel is in
+						(*pix) = colorTable[x];
+						break;
+					case 16: // I dont know what to do with this thing
+						break;
+					case 24:
+						pix->B=filebuffer[row*rowWidth + (y*3) + 0];
+						pix->G=filebuffer[row*rowWidth + (y*3) + 1];
+						pix->R=filebuffer[row*rowWidth + (y*3) + 2];
+						break;
+					case 32:
+						pix->A=filebuffer[row*rowWidth + (y*4) + 0];
+						pix->B=filebuffer[row*rowWidth + (y*4) + 1];
+						pix->G=filebuffer[row*rowWidth + (y*4) + 2];
+						pix->R=filebuffer[row*rowWidth + (y*4) + 3];
+						break;
+				}
 			}
 		}
 	}
@@ -216,9 +247,10 @@ namespace GLOP_IMAGE_BMP{
 			bitsPerPixel = 24;
 		else
 			bitsPerPixel = 32;
-		unsigned int rowWidth = ((bitsPerPixel * (data->width) + 31) / 32)*4;
+
+		unsigned int rowWidth = ((bitsPerPixel * (data->width) + 31) / 32)*4; // width of each scanline in bytes
 		unsigned int temp = rowWidth;
-		if(rowWidth<124) temp=124;
+		if(rowWidth<40) temp=40;
 		unsigned char buff[temp]; // good size buffer
 
 		//===============================
@@ -229,7 +261,7 @@ namespace GLOP_IMAGE_BMP{
 
 		unsigned int filesize=0;
 		filesize+=14; // File Header
-		filesize+=124; // DIB Header
+		filesize+=40; // DIB Header
 		filesize+=0;  // color table
 		if(data->pixelType == GRAY || data->pixelType == RGB){
 			filesize += (data->height * data->width) * 3;
@@ -243,17 +275,17 @@ namespace GLOP_IMAGE_BMP{
 
 		for(int x=0;x<8;x++)buff[6+x]=0; // make sure it is zeros
 
-		buff[10] = 124; // offset to pixel array after the first file header
+		buff[10] = 40+14; // offset to pixel array from the beginning of the file
 
 		fwrite(buff,14,1,imageFP);
 
 		//===============================
 		// DIB Header
 		//===============================
-		for(int x=0 ; x<124 ; x++) // zero the buffer again
+		for(int x=0 ; x<40 ; x++) // zero the buffer again
 			buff[x]=0;
 
-		buff[0] = 124;
+		buff[0] = 40;
 		buff[4*1 + 0] = (data->width >> (8*0)) & 0xff;
 		buff[4*1 + 1] = (data->width >> (8*1)) & 0xff;
 		buff[4*1 + 2] = (data->width >> (8*2)) & 0xff;
@@ -264,25 +296,23 @@ namespace GLOP_IMAGE_BMP{
 		buff[4*2 + 3] = (data->height >> (8*3)) & 0xff;
 
 		buff[4*3 + 0] = 1; // number of planes - whatever this means
-		if(data->pixelType == GRAY || data->pixelType == RGB) // number of bits per pixel
-			buff[4*3 + 2] = 24;
-		else
-			buff[4*3 + 2] = 32;
+		buff[4*3 + 2] = bitsPerPixel; // mark the number of bits per pixel
 
-		buff[4*4 + 0] = 3;
+		buff[4*4 + 0] = 0; // no compression
 
 		//image array size
-		buff[4*5 + 0] = ((rowWidth*data->height)>>(8*0)) & 0xff;
-		buff[4*5 + 1] = ((rowWidth*data->height)>>(8*1)) & 0xff;
-		buff[4*5 + 2] = ((rowWidth*data->height)>>(8*2)) & 0xff;
-		buff[4*5 + 3] = ((rowWidth*data->height)>>(8*3)) & 0xff;
+		// buff[4*5 + 0] = ((rowWidth*data->height)>>(8*0)) & 0xff;
+		// buff[4*5 + 1] = ((rowWidth*data->height)>>(8*1)) & 0xff;
+		// buff[4*5 + 2] = ((rowWidth*data->height)>>(8*2)) & 0xff;
+		// buff[4*5 + 3] = ((rowWidth*data->height)>>(8*3)) & 0xff;
+		buff[4*5 + 0] = 0; //we are allowed to not put a value for this when it is just a listing of RGB values
 
-		buff[4*10 + 3] = 0xFF ; // RED_MASK
-		buff[4*11 + 2] = 0xFF ; // GREEN_MASK
-		buff[4*12 + 1] = 0xFF ; // BLUE_MASK
-		buff[4*13 + 0] = 0xFF ; // ALPHA_MASK
+		// buff[4*10 + 3] = 0xFF ; // RED_MASK
+		// buff[4*11 + 2] = 0xFF ; // GREEN_MASK
+		// buff[4*12 + 1] = 0xFF ; // BLUE_MASK
+		// buff[4*13 + 0] = 0xFF ; // ALPHA_MASK
 
-		fwrite(buff,124,1,imageFP);
+		fwrite(buff,40,1,imageFP);
 
 		//===============================
 		// Write the pixels to the array
